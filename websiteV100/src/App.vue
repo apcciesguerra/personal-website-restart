56d8f8c184fffa2c59c4ca758a10feed5a3fe349
apcciesguerra <ciesguerra2@student.apc.edu.ph>
2025-03-05 22:38:37 +0800

    Installed Timeline.vue

--- websiteV100/src/App.vue
+++ websiteV100/src/App.vue
@@ -1,12 +1,41 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 import { useColorMode, useIntersectionObserver } from '@vueuse/core'
-import ParticlesBg from './components/ParticlesBG.vue'
+import ParticlesBg from './components/ParticlesBg.vue'
 import TextHighlight from './components/TextHighlight.vue'
+import Timeline from './components/Timeline.vue'
 
 const isDark = computed(() => useColorMode().value === "dark")
 const introSection = ref<HTMLElement | null>(null)
 const isIntroVisible = ref(false)
+
+// Timeline data
+const data = [
+  {
+    id: "version1.0",
+    label: "Version 1.0",
+  },
+  {
+    id: "version2.0",
+    label: "Version 2.0",
+  },
+  {
+    id: "version3.0",
+    label: "Version 3.0",
+  },
+  {
+    id: "version4.0",
+    label: "Version 4.0",
+  },
+  {
+    id: "version5.0",
+    label: "Version 5.0",
+  },
+  {
+    id: "version6.0",
+    label: "Version 6.0",
+  },
+];
 
 useIntersectionObserver(introSection, ([{ isIntersecting }]) => {
   isIntroVisible.value = isIntersecting
@@ -60,6 +89,32 @@ useIntersectionObserver(introSection, ([{ isIntersecting }]) => {
           </TextHighlight>
         </h1>
       </div>
+    </div>
+    
+    <!-- Timeline Section -->
+    <div class="h-fit w-full">
+      <Timeline
+        :items="data"
+        title="Beam me up"
+        description="Show the timeline in style"
+      >
+        <template
+          v-for="(item, index) in data"
+          :key="item.id + 'template'"
+          #[item.id]
+        >
+          <div class="relative w-full pl-20 pr-4 md:pl-4">
+            <h3
+              class="mb-4 block text-left text-2xl font-bold text-neutral-500 dark:text-neutral-500"
+            >
+              {{ `Section ${index + 1}` }}
+            </h3>
+          </div>
+          <p class="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            Inspira UI gives you the freedom to design awesome website, in less time.
+          </p>
+        </template>
+      </Timeline>
     </div>
   </main>
 </template>
