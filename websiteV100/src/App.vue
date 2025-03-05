7cd980d22beeb62b5c3b30251598eb5a5fdca068
apcciesguerra <ciesguerra2@student.apc.edu.ph>
2025-03-05 13:47:49 +0800

    Testing Text Highlight effect

--- websiteV100/src/App.vue
+++ websiteV100/src/App.vue
@@ -1,30 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import TextHighlight from './components/TextHighlight.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="text-blance flex justify-center p-10">
+    <h1 class="text-balance text-center text-4xl font-bold">
+      You are a
+      <TextHighlight class="bg-gradient-to-r from-indigo-300 to-purple-300">
+        f****ing great developer !
+      </TextHighlight>
+    </h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
