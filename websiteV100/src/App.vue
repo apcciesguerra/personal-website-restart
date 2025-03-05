6f9b378fa95bf1ef3b9ddbd2ee7d520143f2932a
apcciesguerra <ciesguerra2@student.apc.edu.ph>
2025-03-05 15:22:55 +0800

    Effects testing

--- websiteV100/src/App.vue
+++ websiteV100/src/App.vue
@@ -1,14 +1,27 @@
-<script setup>
-import TextHighlight from './components/TextHighlight.vue';
-</script>
-
 <template>
-  <div class="text-balance flex justify-center p-10">
-    <h1 class="text-balance text-center text-4xl font-bold">
-      Welcome To
-      <TextHighlight class="bg-gradient-to-r from-indigo-300 to-purple-300">
-        My Website
-      </TextHighlight>
-    </h1>
+  <div
+    class="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl"
+  >
+    <span
+      class="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10"
+    >
+      Particles
+    </span>
+    <ParticlesBg
+      class="absolute inset-0"
+      :quantity="100"
+      :ease="100"
+      :color="isDark ? '#FFF' : '#000'"
+      :staticity="10"
+      refresh
+    />
   </div>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useColorMode } from '@vueuse/core'
+import ParticlesBg from './components/ParticlesBg.vue'
+
+const isDark = computed(() => useColorMode().value === "dark")
+</script>
--- websiteV100/tailwind.config.js
+++ websiteV100/tailwind.config.js
@@ -6,7 +6,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ], 
+  ],
   theme: {
     extend: {
       fontFamily: {
