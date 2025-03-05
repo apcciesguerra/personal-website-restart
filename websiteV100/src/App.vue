34f2810e43ae7a6fe1fd2936595247fc10bebb5d
apcciesguerra <ciesguerra2@student.apc.edu.ph>
2025-03-05 15:01:52 +0800

    Yoooo it works

--- websiteV100/src/App.vue
+++ websiteV100/src/App.vue
@@ -2,20 +2,13 @@
 import TextHighlight from './components/TextHighlight.vue';
 </script>
 
-<h1>test</h1>
-
 <template>
-  <div class="text-blance flex justify-center p-10">
+  <div class="text-balance flex justify-center p-10">
     <h1 class="text-balance text-center text-4xl font-bold">
-      You are a
+      Welcome To
       <TextHighlight class="bg-gradient-to-r from-indigo-300 to-purple-300">
-        f****ing great developer!
+        My Website
       </TextHighlight>
     </h1>
   </div>
 </template>
-
-<style>
-@import './style.css';
-@import './output.css';
-</style>
