52f9b0552f54a399aa9cb56aa250397d82d133d1
apcciesguerra <ciesguerra2@student.apc.edu.ph>
2025-03-06 11:28:42 +0800

    Fixed photo4 being cut off when expanded

--- websiteV100/src/components/ExpandableGallery.vue
+++ websiteV100/src/components/ExpandableGallery.vue
@@ -1,27 +1,36 @@
 <template>
-    <div :class="cn('flex h-96 w-full gap-2', props.class)">
-      <div
-        v-for="image in images"
-        :key="image"
-        class="relative flex h-full flex-1 cursor-pointer overflow-hidden rounded-xl transition-all duration-500 ease-in-out hover:flex-[3]"
-      >
-        <img
-          class="relative h-full object-cover"
-          :src="image"
-          :alt="image"
-        />
-      </div>
+  <div :class="cn('flex h-96 w-full gap-2', props.class)">
+    <div
+      v-for="image in images"
+      :key="image"
+      :class="[
+        'relative flex h-full flex-1 cursor-pointer overflow-hidden rounded-xl transition-all duration-500 ease-in-out',
+        isPhoto4(image) ? 'hover:flex-[1.5]' : 'hover:flex-[3]'
+      ]"
+    >
+      <img
+        class="relative h-full w-full object-cover"
+        :src="image"
+        :alt="image"
+        :style="{ objectPosition: 'center' }"
+      />
     </div>
-  </template>
-  
-  <script lang="ts" setup>
-  import type { HTMLAttributes } from "vue";
-  import { cn } from "../lib/utils";
-  
-  interface Props {
-    images: string[];
-    class?: HTMLAttributes["class"];
-  }
-  
-  const props = defineProps<Props>();
-  </script>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { HTMLAttributes } from "vue";
+import { cn } from "../lib/utils";
+
+interface Props {
+  images: string[];
+  class?: HTMLAttributes["class"];
+}
+
+const props = defineProps<Props>();
+
+// Function to check if the image is photo4.jpg
+const isPhoto4 = (imageSrc: string) => {
+  return imageSrc.includes('photo4.jpg');
+};
+</script>
